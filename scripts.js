286fade01ddc1bffd483c8cdd3cd4ec56171b7ec
grzegorz.ziolek <104593451+grzegorz-ziolek@users.noreply.github.com>
2024-06-08 23:52:43 +0200

    revert? scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 // scripts.js
 document.addEventListener("DOMContentLoaded", function() {
     const links = document.querySelectorAll("nav ul li a");
+    const mobileMenu = document.getElementById("mobile-menu");
+    const navList = document.querySelector(".nav-list");
+
     links.forEach(link => {
         link.addEventListener("click", function(e) {
             e.preventDefault();
@@ -9,6 +12,15 @@ document.addEventListener("DOMContentLoaded", function() {
                 top: target.offsetTop,
                 behavior: "smooth"
             });
+
+            // Zamknij menu po kliknięciu na link
+            if (navList.classList.contains("active")) {
+                navList.classList.remove("active");
+            }
         });
+    });
+
+    mobileMenu.addEventListener("click", function() {
+        navList.classList.toggle("active");
     });
 });
